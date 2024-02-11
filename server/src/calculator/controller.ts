ba06fd68ed4881a147b2fb4613c7f039e0a8a721
Hudson Baroni <hudsonbarni@gmail.com>
2024-02-11 20:01:07 -0300

    Aula 01 - Calculator /div

--- server/src/calculator/controller.ts
+++ server/src/calculator/controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 
+
+// sum
 export function sumController(request: Request, response: Response) {
   const body = request.body;
   const num1 = body.a;
@@ -18,7 +20,7 @@ export function sumController(request: Request, response: Response) {
   return response.send({ result });
 }
 
-
+// sub
 export function subController(request: Request, response: Response) {
   const body = request.body;
   const num1 = body.a;
@@ -33,6 +35,24 @@ export function subController(request: Request, response: Response) {
   }
 
   const result = num1 - num2;
+
+  return response.send({ result });
+}
+
+export function divController(request: Request, response: Response) {
+  const body = request.body;
+  const num1 = body.a;
+  const num2 = body.b;
+
+  if (!num1 || !num2) {
+    return response.status(422).send({ message: "a or b must be provided" });
+  }
+
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return response.status(422).send({ message: "a and b must be numbers" });
+  }
+
+  const result = num1 / num2;
 
   return response.send({ result });
 }
--- server/src/calculator/routes.ts
+++ server/src/calculator/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { subController, sumController } from "./controller";
+import { subController, sumController, divController } from "./controller";
 
 const calculatorRoutes = Router();
 
 calculatorRoutes.post("/sum", sumController);
 
 calculatorRoutes.post("/sub", subController);
+
+calculatorRoutes.post("/div", divController);
 
 export default calculatorRoutes;
